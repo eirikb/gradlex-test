57e85680a883a4a5f1b1cb639555114c21628589
Eirik Brandtzæg <eirikb@eirikb.no>
2021-05-14 13:49:39 +0200

    test

---
+++ main-win.c
@@ -0,0 +1,32 @@
+#include "./const.h"
+#include "hash.c"
+#include <stdio.h>
+#include <urlmon.h>
+#pragma comment(lib, "Urlmon.lib")
+
+int __cdecl main() {
+    const char *destFile = "gradlex2";
+
+    char path[1000];
+    snprintf(path, 1000, "http://gradlex.blob.core.windows.net/poc/%s", hash);
+
+    printf("Downloading %s...\n", path);
+    if (S_OK == URLDownloadToFile(NULL, path, destFile, 0, NULL)) {
+        printf("Done!\n");
+    } else {
+        printf("Failed\n");
+        return 1;
+    }
+
+    printf("File downloaded, checking hash...\n");
+    char newHash[128];
+    hashForFile("gradlex2", newHash);
+    printf("Hash: %s\n", newHash);
+    if (strcmp(hash, newHash) != 0) {
+        printf("Hash did not match :(\n");
+        return 1;
+    }
+
+    printf("Done!\n");
+    return 0;
+}
---
+++ stage2.sh
@@ -0,0 +1,13 @@
+echo "Welcome to gradlex (stage 2)!"
+echo "Installing gradle (7.0)...."
+cp -r gradle "$1/.gradle/"
+echo "Done! All is well"
+echo "You should now have gradle-7.0 installed in .gradle"
+echo "Press enter to start gradle:"
+read
+cd "$1"
+ls
+pwd
+ls .gradle
+chmod +x ./.gradle/bin/gradle
+./.gradle/bin/gradle $2
